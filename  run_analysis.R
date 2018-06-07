b2d3732be3f355c2401b9a071a304930daf65fda
alon-tiosano <32680131+alon-tiosano@users.noreply.github.com>
2018-06-07 15:28:39 +0300

    Add files via upload

---  run_analysis.R
+++  run_analysis.R
@@ -5,11 +5,11 @@ download.file(fileUrl,destfile="./Dataset.zip")
 unzip(zipfile="./Dataset.zip",exdir="./Dataset.zip")
 
 # reading data#
-
+#trining#
 x_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
 y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
 subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
-
+#testing#
 x_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
 y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
 subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
@@ -24,7 +24,6 @@ colnames(x_train) <- features[,2]
 colnames(y_train) <-"activityId"
 colnames(subject_train) <- "subjectId"
 
-
 colnames(x_test) <- features[,2] 
 colnames(y_test) <- "activityId"
 colnames(subject_test) <- "subjectId"
@@ -35,24 +34,23 @@ colnames(activityLabels) <- c('activityId','activityType')
 
 mrg_train <- cbind(y_train, subject_train, x_train)
 mrg_test <- cbind(y_test, subject_test, x_test)
-setAllInOne <- rbind(mrg_train, mrg_test)
+All <- rbind(mrg_train, mrg_test)
 
-
-colNames <- colnames(setAllInOne)
+colNamesnew <- colnames(All)
 
 #selecting data#
 
-mean_std <- (grepl("activityId" , colNames) |  grepl("subjectId" , colNames) | grepl("mean" , colNames) |   grepl("std" , colNames) )
+mean_std <- ( grepl("mean" , colNamesnew) | grepl("std" , colNamesnew) | grepl("activityId" , colNamesnew) | grepl("subjectId" , colNamesnew) )
 
 
-setForMeanAndStd <- setAllInOne[ , mean_std == TRUE]
+MeanStd <- All[ , mean_std == TRUE]
 
 
-setWithActivityNames <- merge(setForMeanAndStd, activityLabels, by='activityId', all.x=TRUE)
+Activity_Names <- merge(MeanStd, activityLabels, by='activityId', all.x=TRUE)
 
 # new data#
 
-secTidySet <- aggregate(. ~subjectId + activityId, setWithActivityNames, mean)
-secTidySet <- secTidySet[order(secTidySet$subjectId, secTidySet$activityId),]
+newdata <- aggregate(. ~subjectId + activityId, Activity_Names, mean)
+newdata <- newdata[order(newdata$subjectId, newdata$activityId),]
 
-write.table(secTidySet, "secTidySet.txt", row.name=FALSE)
+write.table(newdata, "newdata.txt", row.name=FALSE)
